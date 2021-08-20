2430c4b0007e2bcec5e48de2c052ca0066cda902
Juan Jose Rodriguez <4skilled@gmail.com>
2021-08-19 21:10:55 -0500

    Se cambiaron a Activies los Compat

---
+++ app/src/main/java/com/cappstudios/yachasun/Profile.kt
@@ -0,0 +1,13 @@
+package com.cappstudios.yachasun
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Profile : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
