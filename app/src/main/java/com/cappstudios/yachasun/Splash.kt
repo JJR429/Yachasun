ed0a6048d7cbedcb8f1ffd2dbaa655d8cf014bac
Juan Jose Rodriguez <4skilled@gmail.com>
2021-08-19 20:12:25 -0500

    Initial commit

---
+++ app/src/main/java/com/cappstudios/yachasun/Splash.kt
@@ -0,0 +1,12 @@
+package com.cappstudios.yachasun
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Splash : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
